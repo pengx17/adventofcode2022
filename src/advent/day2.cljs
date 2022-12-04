2e166bf5d550eb3aec6547890fc7cc665bc7a073
Peng Xiao <pengxiao@freewheel.tv>
2022-12-04 22:34:13 +0800

    day2-p2

--- src/advent/day2.cljs
+++ src/advent/day2.cljs
@@ -1,6 +1,6 @@
 (ns advent.day2
-  (:require [clojure.core.match :refer [match]]
-            [advent.io :refer [load-input]]))
+  (:require [advent.io :refer [load-input]]
+            [clojure.set :as set]))
 
 ;; https://adventofcode.com/2022/day/2
 
@@ -9,37 +9,70 @@ A Y
 B X
 C Z")
 
-(defn win-score [[a b]]
+(defn ->hand-p1 [c]
+  (case c
+    ("A" "X") :rock
+    ("B" "Y") :paper
+    :scissors))
+
+(def hand-index {:rock 0
+                 :paper 1
+                 :scissors 2})
+
+(def index-hand (set/map-invert hand-index))
+
+(defn ->verus [h c]
+  (index-hand (mod (+ (hand-index h) 
+                      (case c
+                        "X" -1
+                        "Y" 0
+                        "Z" 1)) 3)))
+
+(defn compete [a b]
   (* 3
-     (match [a b]
-       ["A" "X"] 1
-       ["B" "X"] 0
-       ["C" "X"] 2
+     (case (- (hand-index a) (hand-index b))
+       0      1
+       (1 -2) 0
+       (-1 2) 2)))
 
-       ["A" "Y"] 2
-       ["B" "Y"] 1
-       ["C" "Y"] 0
+(defn hand->score [h]
+  (case h
+    :rock 1
+    :paper 2
+    :scissors 3))
 
-       ["A" "Z"] 0
-       ["B" "Z"] 2
-       ["C" "Z"] 1)))
+(defn score-p1 [[a b]]
+  (let [h1 (->hand-p1 a)
+        h2 (->hand-p1 b)]
+    (+
+     (compete h1 h2)
+     (hand->score h2))))
 
-(defn score [[a b]]
-  (+ 
-   (win-score [a b])
-   (match b
-       "X" 1
-       "Y" 2
-       "Z" 3)))
+(defn score-p2 [[a b]]
+  (let [h1 (->hand-p1 a)
+        h2 (->verus h1 b)]
+    (+
+     (compete h1 h2)
+     (hand->score h2))))
 
 (defn solve-p1 [input]
   (->> (.split input "\n")
        (filter (complement empty?))
        (map #(.split % " "))
        (map js->clj)
-       (map score)
+       (map score-p1)
+       (reduce +)))
+
+(defn solve-p2 [input]
+  (->> (.split input "\n")
+       (filter (complement empty?))
+       (map #(.split % " "))
+       (map js->clj)
+       (map score-p2)
        (reduce +)))
 
 (comment
   (solve-p1 test-input)
-  (solve-p1 (load-input 2)))
+  (solve-p1 (load-input 2))
+  (solve-p2 test-input)
+  (solve-p2 (load-input 2)))
