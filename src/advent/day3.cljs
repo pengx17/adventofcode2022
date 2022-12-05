94fb1f33747f334afff872e6051a38d64c7eaac4
Peng Xiao <pengxiao@freewheel.tv>
2022-12-05 14:29:26 +0800

    day3

---
+++ src/advent/day3.cljs
@@ -0,0 +1,59 @@
+(ns advent.day3
+  (:require [advent.io :refer [load-input]]
+            [clojure.set :as set]
+            [clojure.string :as string]))
+
+;; https://adventofcode.com/2022/day/3
+
+(def test-input "
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+")
+
+(defn split-row [row]
+  [(subs row 0 (/ (count row) 2))
+   (subs row (/ (count row) 2))])
+
+(defn find-duplicate 
+  ([xs ys]
+   (let [set-xs (set xs)]
+     (set (filter set-xs ys))))
+  ([xs ys zs]
+   (find-duplicate zs (find-duplicate xs ys))))
+
+(defn ->score [c]
+  (let [code (.charCodeAt c 0)
+        [a z] [(.charCodeAt "a" 0) (.charCodeAt "z" 0)]
+        A (.charCodeAt "A" 0)]
+    (if (and (>= code a) (<= code z))
+      (inc (- code a))
+      (+ 27 (- code A)))))
+
+(defn solve-p1 [input]
+  (->> (string/split-lines input)
+       (filter (complement empty?))
+       (map string/trim)
+       (map split-row)
+       (map #(apply find-duplicate %))
+       (reduce into [])
+       (map ->score)
+       (reduce +)))
+
+(defn solve-p2 [input]
+  (->> (string/split-lines input)
+       (filter (complement empty?))
+       (map string/trim)
+       (partition 3)
+       (map #(apply find-duplicate %))
+       (map first)
+       (map ->score)
+       (reduce +)))
+
+(comment
+  (solve-p1 test-input)
+  (solve-p1 (load-input 3))
+  (solve-p2 (load-input 3)))
